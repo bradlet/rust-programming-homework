d561b102e1826b91d4155e298254092bcb5bb135
bradlet <bthompson@tradewelltech.co>
2023-04-08 14:24:10 -0700

    Import my library and ignore all homework dir build output

--- hw1/src/main.rs
+++ hw1/src/main.rs
@@ -1,3 +1,10 @@
+/// HW1
+/// Bradley Thompson
+
+use bradleys_random_rust_helpers::*;
+use text_colorizer::Color;
+
 fn main() {
     println!("Hello, world!");
+	horizontal_sep(16, Some(Color::Green));
 }
